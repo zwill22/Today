d49f714efdf08ab98ddf2b17c310e23c0f51d7b1
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-09-05 12:50:20 +0100

    Added sections for editing

---
+++ Today/DetailViewController/ReminderViewControllerSection.swift
@@ -0,0 +1,30 @@
+//
+//  ReminderViewControllerSection.swift
+//  Today
+//
+//  Created by Zack Williams on 05/09/2024.
+//
+
+import Foundation
+
+extension ReminderViewController {
+    enum Section: Int, Hashable {
+        case view
+        case title
+        case date
+        case notes
+        
+        var name: String {
+            switch self {
+            case .view:
+                return ""
+            case .title:
+                return NSLocalizedString("Title", comment: "Title section name")
+            case .date:
+                return NSLocalizedString("Date", comment: "Date section name")
+            case .notes:
+                return NSLocalizedString("Notes", comment: "Notes section name")
+            }
+        }
+    }
+}
