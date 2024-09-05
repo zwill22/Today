c132640e06be240a0db6f66da24c3ffb88a7366f
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-09-05 13:57:49 +0100

    Conform to content view protocol

---
+++ Today/ContentViews/UIContentConfigurationStateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfigurationStateless.swift
+//  Today
+//
+//  Created by Zack Williams on 05/09/2024.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
