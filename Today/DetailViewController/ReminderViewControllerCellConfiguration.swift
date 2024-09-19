8a93386818c41d0bbd7235ba64062bcb5afa9906
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-09-19 17:37:24 +0100

    Made the date configuration editable

--- Today/DetailViewController/ReminderViewControllerCellConfiguration.swift
+++ Today/DetailViewController/ReminderViewControllerCellConfiguration.swift
@@ -37,6 +37,9 @@ extension ReminderViewController {
     func dateConfiguration(for cell: UICollectionViewListCell, with date: Date) -> DatePickerContentView.Configuration {
         var contentConfiguration = cell.datePickerConfiguration()
         contentConfiguration.date = date
+        contentConfiguration.onChange = { [weak self] dueDate in
+            self?.workingReminder.dueDate = dueDate
+        }
         
         return contentConfiguration
     }
