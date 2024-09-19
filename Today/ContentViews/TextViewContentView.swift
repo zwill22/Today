67d7334b27a1f9573e3340665f460a1a01424bb8
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-09-19 17:41:30 +0100

    Made the notes configuration editable

--- Today/ContentViews/TextViewContentView.swift
+++ Today/ContentViews/TextViewContentView.swift
@@ -10,6 +10,7 @@ import UIKit
 class TextViewContentView: UIView, UIContentView {
     struct Configuration: UIContentConfiguration {
         var text: String? = ""
+        var onChange: (String) -> Void = { _ in }
         
         func makeContentView() -> any UIView & UIContentView {
             return TextViewContentView(self)
@@ -33,6 +34,7 @@ class TextViewContentView: UIView, UIContentView {
         
         addPinnedSubview(textView, height: 200)
         textView.backgroundColor = nil
+        textView.delegate = self
         textView.font = UIFont.preferredFont(forTextStyle: .body)
     }
     
@@ -49,5 +51,12 @@ class TextViewContentView: UIView, UIContentView {
 extension UICollectionViewListCell {
     func textViewConfiguration() -> TextViewContentView.Configuration {
         TextViewContentView.Configuration()
+    }
+}
+
+extension TextViewContentView: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        guard let configuration = configuration as? TextViewContentView.Configuration else { return }
+        configuration.onChange(textView.text)
     }
 }
