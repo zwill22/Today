64bd613d05e76c0541b9ba64c1eb2e458b8360c9
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-09-23 15:15:10 +0100

    Added readAll reminders function

--- Today/Models/TodayError.swift
+++ Today/Models/TodayError.swift
@@ -8,11 +8,15 @@
 import Foundation
 
 enum TodayError: LocalizedError {
+    case accessDenied
     case failedReadingReminders
     case reminderHasNoDueDate
     
     var errorDescription: String? {
         switch self {
+        case .accessDenied:
+            return NSLocalizedString(
+                "The app does not have permission to read reminders", comment: "access denied error description")
         case .failedReadingReminders:
             return NSLocalizedString("Failed to read reminders.", comment: "failed reading reminders error description")
         case .reminderHasNoDueDate:
