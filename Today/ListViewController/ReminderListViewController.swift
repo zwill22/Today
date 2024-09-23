c6982a2a4b7389e0d1156dfd6bf4cd946c55fa0c
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-09-23 15:31:49 +0100

    Added error message

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -119,6 +119,24 @@ class ReminderListViewController: UICollectionViewController {
         navigationController?.pushViewController(viewController, animated: true)
     }
     
+    func showError(_ error: Error) {
+        let alertTitle = NSLocalizedString("Error", comment: "Error alert title")
+        let alert = UIAlertController(
+            title: alertTitle, message: error.localizedDescription, preferredStyle: .alert)
+        
+        let actionTitle = NSLocalizedString("OK", comment: "Alert OK button title")
+        alert.addAction(
+            UIAlertAction(
+                title: actionTitle, style: .default,
+                handler: { [weak self] _ in
+                    self?.dismiss(animated: true)
+                }
+            )
+        )
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
     private func listLayout() -> UICollectionViewCompositionalLayout {
         var listConfiguration = UICollectionLayoutListConfiguration(
             appearance: .grouped
