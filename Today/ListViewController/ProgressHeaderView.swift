e42f276dc0a2b8e9561e69b62884988786656d77
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-09-23 13:36:50 +0100

    Added ProgressHeaderView

---
+++ Today/ListViewController/ProgressHeaderView.swift
@@ -0,0 +1,76 @@
+//
+//  ProgressHeaderView.swift
+//  Today
+//
+//  Created by Zack Williams on 23/09/2024.
+//
+
+import UIKit
+
+class ProgressHeaderView: UICollectionReusableView {
+    static var elementKind: String { UICollectionView.elementKindSectionHeader }
+    
+    var progress: CGFloat = 0 {
+        didSet {
+            heightConstraint?.constant = progress * bounds.height
+            UIView.animate(withDuration: 0.2) { [weak self] in
+                self?.layoutIfNeeded()
+            }
+        }
+    }
+    
+    private let upperView = UIView(frame: .zero)
+    private let lowerView = UIView(frame: .zero)
+    private let containerView = UIView(frame: .zero)
+    private var heightConstraint: NSLayoutConstraint?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        prepareSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        containerView.layer.masksToBounds = true
+        containerView.layer.cornerRadius = 0.5 * containerView.bounds.width
+    }
+    
+    private func prepareSubviews() {
+        containerView.addSubview(upperView)
+        containerView.addSubview(lowerView)
+        addSubview(containerView)
+        
+        upperView.translatesAutoresizingMaskIntoConstraints = false
+        lowerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true
+        containerView.heightAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 1).isActive = true
+        
+        containerView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        containerView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+    
+        containerView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.85).isActive = true
+        
+        upperView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        upperView.bottomAnchor.constraint(equalTo: lowerView.topAnchor).isActive = true
+        lowerView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
+        upperView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        upperView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        lowerView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        lowerView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        
+        heightConstraint = lowerView.heightAnchor.constraint(equalToConstant: 0)
+        heightConstraint?.isActive = true
+        
+        backgroundColor = .clear
+        containerView.backgroundColor = .clear
+        upperView.backgroundColor = .todayProgressUpperBackground
+        lowerView.backgroundColor = .todayProgressLowerBackground
+    }
+}
