a1fecbb1b1b549cec46419db5984f8085be04cb7
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-09-23 14:59:59 +0100

    Added function to fetch reminders asynchronously

---
+++ Today/Models/EKEventStoreAsyncFetch.swift
@@ -0,0 +1,23 @@
+//
+//  EKEventStoreAsyncFetch.swift
+//  Today
+//
+//  Created by Zack Williams on 23/09/2024.
+//
+
+import EventKit
+import Foundation
+
+extension EKEventStore {
+    func reminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
+        try await withCheckedThrowingContinuation { continuation in
+            fetchReminders(matching: predicate) { reminders in
+                if let reminders {
+                    continuation.resume(returning: reminders)
+                } else {
+                    continuation.resume(throwing: TodayError.failedReadingReminders)
+                }
+            }
+        }
+    }
+}
